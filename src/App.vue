2d4598284b09f58c762db837a59fc032a77a351d
Matt Jones <Matt.Jones@1ststop.co.uk>
2020-04-30 10:15:08 +0100

    Initial commit, overview component added

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="game">
+    <Overview />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Overview from "./components/Overview";
+export default {
+  name: "app",
+  components: {
+    Overview
+  },
+  methods: {
+    coding() {
+      // DO SOMETHING
+    },
+    loop() {
+      // GAME LOOP
+      requestAnimationFrame(this.loop);
+    }
+  },
+  cretaed() {
+    this.loop();
+    window.addEventListener("keypress", this.coding);
+  },
+  destroyed() {
+    window.removeEventListener("keypress", this.coding);
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+</style>
---
+++ src/components/Overview.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="overview">
+    <h3>Current Bytes</h3>
+    <div class="bytes">{{ bytes }}</div>
+    <div class="bps">Bytes per second {{ bps }}</div>
+    <div class="level">Current level: {{ level }}</div>
+    <div class="next-level">Bytes until next level: {{ bytesUntilNextLevel }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "overview",
+  computed: {
+    bytes() {
+      return Math.round(this.$store.state.bytes);
+    },
+    bps() {
+      return this.$store.state.bps.toFixed(1);
+    },
+    level() {
+      return this.$store.state.level;
+    },
+    bytesUntilNextLevel() {
+      return this.$store.getters.bytesUntilLevelUp;
+    }
+  }
+};
+</script>
